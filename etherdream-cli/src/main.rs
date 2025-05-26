e8169d71b6836ef4a9e86ac677f2c3c7e5c5f382
Justin Martin <justinbmartin@gmail.com>
2025-05-26 09:57:43 -0400

    cli clean-up

--- etherdream-cli/src/main.rs
+++ etherdream-cli/src/main.rs
@@ -1,24 +1,24 @@
 use std::collections::HashMap;
-use std::io::{ stdin, stdout, Write };
+use std::io::{ self, Write };
 use std::net::SocketAddr;
 use std::sync::Arc;
 
-use clap::{ Parser, Subcommand };
+use clap::{ self, Parser };
 use parking_lot::RwLock;
 
-use etherdream::{ Client, Device, discovery };
+use etherdream;
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  CLI
 
 /// Discover and manage Etherdream devices.
-#[derive( Debug, Parser )]
+#[derive( Debug, clap::Parser )]
 #[command( multicall=true )]
 struct Cli {
   #[command( subcommand )]
   command: Commands,
 }
 
-#[derive( Debug, Subcommand )]
+#[derive( Debug, clap::Subcommand )]
 enum Commands {
   /// List discovered devices.
   #[clap( alias = "ls" )]
@@ -48,128 +48,126 @@ enum Commands {
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - Main
 
+type StateRef = Arc<RwLock<State>>;
+
 struct State {
   // The current client that is active
   current_client: Option<usize>,
 
   // A map of actively running Etherdream clients
-  clients: HashMap<usize,Client>,
+  clients: HashMap<usize,etherdream::Client>,
 
   // A list of all discovered Etherdream devices (using a vec to allow accessing by index)
-  devices: Vec<( SocketAddr, Device )>
+  devices: Vec<( SocketAddr, etherdream::Device )>
 }
 
 #[tokio::main]
 async fn main() {
-  let state = Arc::new( RwLock::new( State{ clients: HashMap::new(), current_client: None, devices: Vec::new() }));
   let mut input = String::new();
-
-  // Start the Etherdream discovery service
+  let state = Arc::new( RwLock::new( State{ clients: HashMap::new(), current_client: None, devices: Vec::new() }));
+  
+  // Start the Etherdream discovery service. All discovered devices will be
+  // stored in `state.devices`.
   tokio::task::spawn({
     let state = state.clone();
 
     async move {
-      return discovery::Server::new()
-        .serve( move | address, device |{
-          state.write().devices.push( ( address, device ) );
-        })
+      return etherdream::discovery::Server::new()
+        .serve( move | address, device | { state.write().devices.push( ( address, device ) ); })
         .await;
     }
   });
 
-  // Start the REPL (via a naive loop)
+  // Start the REPL
   loop {
-    match process_command( &mut input, &state ).await {
-      Ok( quit ) => if quit { break; } else { continue; },
-      Err( msg ) => println!( "{msg}" )
+    // Print console prefix, one of "> " or "[<device index>] >"
+    let prefix = if let Some( index ) = state.read().current_client { format!( "[{}] ", index ) } else { String::from( "" ) };
+    print!( "{}> ", prefix );
+  
+    // Read and parse console input
+    input.clear();
+    let _ = io::stdout().flush();
+    let _ = io::stdin().read_line( &mut input );
+  
+    if let Some( args ) = shlex::split( input.trim() ) {
+      if let Ok( cli ) = Cli::try_parse_from( args ) {
+
+        match cli.command {
+          Commands::Connect{ index } => do_connect_device( &state, index ).await,
+          Commands::Device{ index } => do_inspect_device( &state, index ),
+          Commands::Exit => break,
+          Commands::List => do_list_devices( &state ),
+          Commands::Ping => do_ping_device( &state )
+        }
+      }
     }
   }
 }
 
-async fn process_command( input: &mut String, state: &Arc<RwLock<State>> ) -> Result<bool,String> {
-  input.clear();
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - Command Handlers
 
-  // Print console prefix (one of "> " or "[<device index>] >")
-  let prefix = if let Some( index ) = state.read().current_client { format!( "[{}] ", index ) } else { String::from( "" ) };
-  print!( "{}> ", prefix );
-  
-  // Read and parse console input
-  stdout().flush().map_err( |e|{ format!( "Failed to flush output: {}", e.to_string() ) })?;
-  stdin().read_line( input ).map_err( |e| format!( "Failed to read input: {}", e.to_string() ))?;
-  
-  let args = shlex::split( input.trim() ).ok_or( "Failed to read input." )?;
-  let cli = Cli::try_parse_from( args ).map_err( |e| e.to_string() )?;
+fn do_list_devices( state: &StateRef ) {
+  let state = state.read();
 
-  // TODO: taking write-access here...
+  if state.devices.is_empty() {
+    println!( "(no devices)" );
+  } else {
+    for ( index, ( address, device ) ) in state.devices.iter().enumerate() {
+      println!( "  [{}] {} (MAC: {})", index, address, device.mac_address() );
+    }
+  }
+}
+
+async fn do_connect_device( state: &StateRef, device_index: usize ) {
   let mut state = state.write();
 
-  // Execute the user-provided command
-  match cli.command {
-    Commands::Connect{ index } => {
-      if state.clients.contains_key( &index ) {
-        state.current_client = Some( index );
-        Ok( false )
+  if state.clients.contains_key( &device_index ) {
+    state.current_client = Some( device_index );
 
-      } else {
-        match state.devices.get( index ) {
-          Some( ( address, _ ) ) => {
-            match Client::start( *address, |_, command, _|{ println!( "COMMAND RECEIVED: {:?}", command ) }).await {
-              Ok( client ) => {
-                state.clients.insert( index, client );
-                state.current_client = Some( index );
-    
-                return Ok( false );
-              }
+  } else {
+    match state.devices.get( device_index ) {
+      Some( ( address, _ ) ) => {
+        match etherdream::Client::start( *address, |_, command, _|{ println!( "COMMAND RECEIVED: {:?}", command ) }).await {
+          Ok( client ) => {
+            state.clients.insert( device_index, client );
+            state.current_client = Some( device_index );
+          }
 
-              Err( msg ) => {
-                return Err( String::from( format!( "Failed to connect to Etherdream DAC: {}", msg ) ) ); 
-              }
-            }
-          },
-
-          None => { 
-            return Err( String::from( "(does not exist)" ) ); 
+          Err( msg ) => {
+            println!( "Failed to connect to Etherdream DAC: {}", msg ); 
           }
         }
+      },
+
+      None => { 
+        println!( "(does not exist)" ); 
       }
+    }
+  }
+}
+
+fn do_inspect_device( state: &StateRef, device_index: usize ) {
+  match state.read().devices.get( device_index ) {
+    Some(( address, device )) => {
+      println!( "Address = {address}\n" );
+      println!( "{device}" );
     },
-
-    Commands::Ping => {
-      if let Some( client_index ) = state.current_client {
-        let client = state.clients.get( &client_index ).unwrap();
-        let _ = client.ping();
-      }
-
-      return Ok( false );
+    None => { 
+      println!( "(does not exist)" ); 
     }
+  }
+}
 
-    Commands::Device{ index } => {
-      match state.devices.get( index ) {
-        Some(( address, device )) => {
-          println!( "Address = {address}\n" );
-          println!( "{device}" );
-          return Ok(false);
-        },
-        None => { 
-          return Err( String::from( "(does not exist)" ) ); 
-        }
-      }
-    }
+fn do_ping_device( state: &StateRef ) {
+  let state = state.read();
 
-    Commands::List => {
-      if state.devices.is_empty() {
-        println!( "(no devices)" );
-      } else {
-        for ( index, ( address, device ) ) in state.devices.iter().enumerate() {
-          println!( "  [{}] {} (MAC: {})", index, address, device.mac_address() );
-        }
-      }
+  if let Some( client_index ) = state.current_client {
+    let client = state.clients.get( &client_index ).unwrap();
+    let _ = client.ping();
+    
+    //
 
-      return Ok( false );
-    }
-
-    Commands::Exit => {
-      return Ok( true );
-    }
+  } else {
+    println!( "(no device connected)" );
   }
 }
