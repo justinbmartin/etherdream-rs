3cc666d0a8f05e94ec9843c953b6705c085f4c5b
Justin Martin <justinbmartin@gmail.com>
2026-05-24 18:28:16 -0400

    simulator now sends broadcasts

--- etherdream-simulator/src/main.rs
+++ etherdream-simulator/src/main.rs
@@ -6,7 +6,10 @@ use etherdream_simulator::Simulator;
 #[tokio::main]
 async fn main() -> ExitCode {
   match Simulator::start().await {
-    Ok( simulator ) => println!( "Listening on: {}", simulator.address() ),
+    Ok( simulator ) => {
+      println!( "Listening on: {}", simulator.address() );
+      simulator.stop().await;
+    },
     Err( err ) => eprintln!( "Failed to start simulator: {}", err )
   }
 
--- etherdream-simulator/src/simulator.rs
+++ etherdream-simulator/src/simulator.rs
@@ -7,10 +7,12 @@
 use std::io;
 use std::net::{ IpAddr, Ipv4Addr, SocketAddr };
 use std::sync::{ Arc, RwLock };
+use std::time::Duration;
 
 use tokio::io::{ AsyncReadExt, AsyncWriteExt };
-use tokio::net::TcpListener;
+use tokio::net::{ TcpListener, UdpSocket };
 use tokio::task;
+use tokio::time::interval;
 
 use etherdream::protocol;
 
@@ -20,12 +22,14 @@ const DEFAULT_POINT_BUFFER_CAPACITY: u16 = 1024;
 pub struct Simulator {
   // The local socket address that the simulator is communicating on.
   address: SocketAddr,
+  // ...
+  _broadcast_handle: task::JoinHandle<io::Result<()>>,
   // The intrinsic properties of the device associated with this simulator.
   intrinsics: protocol::Intrinsics,
   // The real-time device state associated with this simulator.
   state: Arc<RwLock<protocol::State>>,
   // The join handle that owns the asynchronous server task.
-  _handle: task::JoinHandle<io::Result<()>>
+  handle: task::JoinHandle<io::Result<()>>
 }
 
 impl Simulator {
@@ -155,11 +159,65 @@ impl Simulator {
       }
     });
 
+    let broadcast_handle = tokio::spawn({
+      let socket = UdpSocket::bind( SocketAddr::new( IpAddr::V4( Ipv4Addr::LOCALHOST ), 0 ) ).await?;
+      socket.set_broadcast( true )?;
+
+      let broadcast_addr = SocketAddr::new( IpAddr::V4( Ipv4Addr::LOCALHOST ), protocol::BROADCAST_PORT );
+      let mut interval = interval( Duration::from_secs( 1 ) );
+
+      let intrinsics = intrinsics.clone();
+      let state = state.clone();
+      let mut tx_buf = [0u8; protocol::BROADCAST_BYTES_SIZE];
+
+      async move {
+        loop {
+          interval.tick().await;
+
+          tx_buf[0..6].copy_from_slice( intrinsics.mac_address.as_slice() );
+          tx_buf[6..8].copy_from_slice( &intrinsics.version.hardware.to_le_bytes() );
+          tx_buf[8..10].copy_from_slice( &intrinsics.version.software.to_le_bytes() );
+          tx_buf[10..12].copy_from_slice( &intrinsics.buffer_capacity.to_le_bytes() );
+          tx_buf[12..16].copy_from_slice( &intrinsics.max_points_per_second.to_le_bytes() );
+
+          if let Ok( state ) = state.read() {
+            tx_buf[16] = 0;
+            tx_buf[17] = match state.light_engine_state {
+              protocol::LightEngineState::Ready => 0,
+              protocol::LightEngineState::WarmUp => 1,
+              protocol::LightEngineState::CoolDown => 2,
+              protocol::LightEngineState::Estop => 3
+            };
+
+            tx_buf[18] = match state.playback_state {
+              protocol::PlaybackState::Idle => 0,
+              protocol::PlaybackState::Prepared => 1,
+              protocol::PlaybackState::Playing => 2
+            };
+
+            tx_buf[19] = match state.source {
+              protocol::Source::Network => 0,
+              protocol::Source::Ilda => 1,
+              protocol::Source::Internal => 2,
+            };
+
+            tx_buf[20..26].fill( 0 );
+            tx_buf[26..28].copy_from_slice( &state.points_buffered.to_le_bytes() );
+            tx_buf[28..32].copy_from_slice( &state.points_per_second.to_le_bytes() );
+            tx_buf[32..36].copy_from_slice( &state.points_lifetime.to_le_bytes() );
+          }
+
+          socket.send_to( &tx_buf, broadcast_addr ).await?;
+        }
+      }
+    });
+
     Ok( Self{
       address,
+      _broadcast_handle: broadcast_handle,
+      handle,
       intrinsics,
-      state,
-      _handle: handle
+      state
     })
   }
 
@@ -187,5 +245,9 @@ impl Simulator {
     if let Ok( mut state ) = self.state.write() {
       state.points_buffered = state.points_buffered.saturating_sub( count );
     }
+  }
+
+  pub async fn stop( self ) {
+    let _ = self.handle.await;
   }
 }
